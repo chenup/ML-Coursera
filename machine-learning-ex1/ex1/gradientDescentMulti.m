54c35504e9ca57584ac032e6c00e26ed6cd333b5
rex clq <420087213@qq.com>
2018-03-18 18:28:05 +0800

    2018/3/18

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -16,8 +16,11 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
+  num = size(theta)(1);
+  for i = 1 : num
+      Z = X * theta - y;
+      theta(i) = theta(i) - alpha * (Z' * X(:,i)) / m;
+  end
 
 
 
